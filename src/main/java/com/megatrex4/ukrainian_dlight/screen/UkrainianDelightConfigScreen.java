f209d21b57cc2c102537ae1b3baa0bd5d25d5c94
Eugene <skyzgamer@outlook.com>
2024-07-17 21:59:32 +0300

    added configuration

---
+++ src/main/java/com/megatrex4/ukrainian_dlight/screen/UkrainianDelightConfigScreen.java
@@ -0,0 +1,30 @@
+package com.megatrex4.ukrainian_dlight.screen;
+
+import com.megatrex4.ukrainian_dlight.UkrainianDelight;
+import com.megatrex4.ukrainian_dlight.config.ModConfig;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import me.shedaniel.clothconfig2.gui.ClothConfigScreen;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+
+public class UkrainianDelightConfigScreen {
+    public static Screen getScreen(Screen parent) {
+        ConfigBuilder builder = ConfigBuilder.create()
+                .setParentScreen(parent)
+                .setTitle(UkrainianDelight.i18n("title.config"));
+
+        ConfigCategory general = builder.getOrCreateCategory(UkrainianDelight.i18n("category.general"));
+        ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+
+        general.addEntry(entryBuilder.startIntField(UkrainianDelight.i18n("option.brewing_keg_capacity"), ModConfig.brewingKegCapacity)
+                .setDefaultValue(20000)
+                .setSaveConsumer(newValue -> ModConfig.brewingKegCapacity = newValue)
+                .build());
+
+        builder.setSavingRunnable(ModConfig::saveConfig);
+
+        return builder.build();
+    }
+}
